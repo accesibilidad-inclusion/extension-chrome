1eb22e24ff3a4f10f97ce5a730983df719fb4a79
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-05-31 17:36:56 -0400

    fix button color

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,13 @@
         </main>
         <div class="fixed bottom-0 left-0 w-full bg-white shadow-lg p-4 flex justify-center space-x-4 h-[70px]">
             <button v-if="$route.path === '/'"
-                class="bg-yellow-500 text-white py-2 px-4 rounded hover:bg-blue-600 flex-1">
+                class="bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-600 flex-1">
                 <router-link to="/recording">
                     Ir a interfaz de grabación
                 </router-link>
             </button>
             <button v-if="$route.path === '/recording'">
-                <router-link to="/" class="bg-red-500 text-white py-2 px-4 rounded hover:bg-blue-600 flex-1">
+                <router-link to="/" class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 flex-1">
                     Volver a inicio
                 </router-link>
             </button>
