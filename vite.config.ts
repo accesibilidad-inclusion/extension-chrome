59451e7b4e9fbecdbd92f78be48bb644a05ab0a6
NivekNK <kevin.rojas.v@mail.pucv.cl>
2024-06-02 19:33:47 -0400

    Hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,21 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json" assert { type: "json" };
 
-// https://vitejs.dev/config/
+import hotReloadExtension from "hot-reload-extension-vite";
+
 export default defineConfig({
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
-    plugins: [vue(), vueJsx(), crx({ manifest })],
+    plugins: [
+        vue(),
+        vueJsx(),
+        crx({ manifest }),
+        hotReloadExtension({
+            log: true,
+            backgroundPath: "src/service-worker.ts",
+        }),
+    ],
 });
