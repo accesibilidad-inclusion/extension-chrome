55e15cac5786de230d84fb7c9bb463fd51e44088
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-06-07 12:39:04 -0400

    Arreglado error en botones de interfaz de grabacion
    Al haber router links dentro de botones, sucedía que no toda la interfaz del boton te redirigía a la página de grabación.

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,16 @@
                 <RouterView />
             </div>
         </main>
-        <div class="fixed bottom-0 left-0 w-full bg-white shadow-lg p-4 flex justify-center space-x-4 h-[70px]">
-            <button v-if="$route.path === '/'"
-                class="bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-600 flex-1">
-                <router-link to="/recording">
-                    Ir a interfaz de grabación
-                </router-link>
-            </button>
-            <button v-if="$route.path === '/recording'">
-                <router-link to="/" class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 flex-1">
-                    Volver a inicio
-                </router-link>
-            </button>
+        <div
+            class="fixed bottom-0 left-0 w-full bg-white shadow-lg p-4 flex justify-center items-center space-x-4 h-[70px]">
+            <router-link v-if="$route.path === '/'" to="/recording"
+                class="bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-600 w-full text-center font-medium">
+                Ir a interfaz de grabación
+            </router-link>
+            <router-link v-if="$route.path === '/recording'" to="/"
+                class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 w-full text-center font-medium">
+                Volver a inicio
+            </router-link>
         </div>
     </div>
 
