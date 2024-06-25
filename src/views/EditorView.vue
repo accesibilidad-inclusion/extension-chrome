732b06d320ae6107432b9f71c93cbbc9cb92a23f
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-06-25 16:35:33 -0400

    Obtener pasos del editor desde el local storage y nuevo diseño
    Ahora, los pasos en el editor se obtienen desde el local storage, lo que significa que ya no se depende de la implementación anterior. Además, se implementa un nuevo diseño básico de editor, con un botón que dice "Editar"

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,31 +1,42 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import type { Step } from "@/scripts/types";
 
 const steps = ref<Step[]>([]);
 
-chrome.runtime.sendMessage(
-    {
-        action: "pictos__get-editor-data",
-    },
-    (data: { steps: Step[] }) => {
-        steps.value = data.steps;
-    },
-);
+onMounted(() => {
+    const savedSteps = localStorage.getItem("pictos_steps");
+    if (savedSteps) {
+        steps.value = JSON.parse(savedSteps);
+    }
+});
 </script>
 
 <template>
-    <ul class="mt-4 flex flex-col gap-6" id="screenshots-container">
-        <li v-for="(step, index) in steps" :key="index">
-            <p class="text-lg mb-2">Paso {{ step.counter }}</p>
-            <p class="mb-4">{{ step.screenshotData.description }}</p>
-            <div class="relative">
+    <div class="max-w-2xl mx-auto my-12">
+        <div class="flex justify-between items-center mb-8">
+            <h1 class="text-3xl font-semibold">Editor de pasos</h1>
+            <button class="px-5 py-2 bg-blue-500 text-white rounded flex items-center gap-2">
+                <img src="/assets/edit.svg" alt="edit-icon" class="w-4 h-4" />
+                <span>Editar</span>
+            </button>
+        </div>
+        <ul class="mt-4 flex flex-col gap-8" id="screenshots-container">
+            <li v-for="(step, index) in steps" :key="index">
+                <div class="flex items-center gap-4 mb-5">
+                    <div class="w-9 h-9 rounded-full bg-gray-200 flex justify-center items-center">
+                        <span class="text-lg">
+                            {{ step.counter }}
+                        </span>
+                    </div>
+                    <p class="text-lg font-medium">{{ step.screenshotData.description }}</p>
+                </div>
                 <img
                     :src="step.screenshotUrl"
-                    class="w-full h-auto"
+                    class="w-full h-auto rounded border"
                     :alt="step.screenshotData.description"
                 />
-            </div>
-        </li>
-    </ul>
+            </li>
+        </ul>
+    </div>
 </template>
