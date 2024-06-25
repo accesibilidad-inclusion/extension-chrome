6c18b3aa6e6ad7eabdb8f12b13491f4adcdb0c7e
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-06-25 16:39:39 -0400

    Añadido estado reactivo al botón de editar
    Esto hará que cambie el texto y que cambie de color. La variable "isEditing" es la encargada de gestionar el estado

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from "vue";
 import type { Step } from "@/scripts/types";
 
 const steps = ref<Step[]>([]);
+const isEditing = ref(false);
 
 onMounted(() => {
     const savedSteps = localStorage.getItem("pictos_steps");
@@ -16,9 +17,13 @@ onMounted(() => {
     <div class="max-w-2xl mx-auto my-12">
         <div class="flex justify-between items-center mb-8">
             <h1 class="text-3xl font-semibold">Editor de pasos</h1>
-            <button class="px-5 py-2 bg-blue-500 text-white rounded flex items-center gap-2">
+            <button
+                @click="isEditing = !isEditing"
+                class="px-5 py-2 bg-blue-500 text-white rounded flex items-center gap-2"
+                :class="[isEditing ? 'bg-red-500' : 'bg-blue-500']"
+            >
                 <img src="/assets/edit.svg" alt="edit-icon" class="w-4 h-4" />
-                <span>Editar</span>
+                <span>{{ isEditing ? "Dejar de editar" : "Editar" }}</span>
             </button>
         </div>
         <ul class="mt-4 flex flex-col gap-8" id="screenshots-container">
