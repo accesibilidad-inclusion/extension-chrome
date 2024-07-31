ce3f4967f9c7594c6159abebbd763781fa6b6104
Patrick <patricio@edipro.cl>
2024-07-30 20:07:46 -0400

    res

--- src/router.ts
+++ src/router.ts
@@ -3,11 +3,13 @@ import { createMemoryHistory, createRouter } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import RecordingView from "@/views/RecordingView.vue";
 import EditorView from "@/views/EditorView.vue";
+import ElementosView from "@/views/ElementosView.vue";
 
 const routes = [
     { path: "/", name: "HomeView", component: HomeView },
     { path: "/recording", name: "RecordingView", component: RecordingView },
     { path: "/editor", name: "EditorView", component: EditorView },
+    { path: "/elementos", name: "ElementosView", component: ElementosView },
 ];
 
 const router = createRouter({
