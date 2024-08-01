dfc59d2a9171239eddda6e983cfd08d77a492458
Patrick <patricio@edipro.cl>
2024-08-01 04:36:41 -0400

    update
    update flujo

--- src/views/RecordingView.vue
+++ src/views/RecordingView.vue
@@ -8,6 +8,7 @@ import {
     removeGuideFromLocalStorage,
     saveGuideToLocalStorage,
 } from "@/utils/chrome-utils";
+import { useRouter } from "vue-router";
 
 const guide = ref<Guide>({
     title: getMessage("taskDefaultName"),
@@ -18,6 +19,14 @@ const guide = ref<Guide>({
 });
 
 const currentStepIndex = ref(0);
+// Definir variables reactivas
+const isSpeaking = ref(false);
+const showAlert = ref(false);
+const iconClass = ref('icon-voz');
+const activeVoice = ref<string | null>(null);
+const utterance = ref<SpeechSynthesisUtterance | null>(null);
+
+
 
 const step = computed(() => {
     if (guide.value.steps.length <= 0) return undefined;
@@ -38,6 +47,7 @@ const nextStep = () => {
 };
 
 const images = ref<HTMLImageElement[]>([]);
+const router = useRouter();
 
 const saveGuide = () => {
     saveGuideToLocalStorage(guide.value);
@@ -123,6 +133,7 @@ const cutoutStyle = (data: FocusData, index: number) => {
     if (!data || data.radius <= 0 || index >= images.value.length) return {};
 
     const img = images.value[index];
+    if (!img) return;
 
     const x = (data.x * img.width) / 100;
     const y = (data.y * img.height) / 100;
@@ -152,77 +163,249 @@ const openEditor = () => {
         });
     });
 };
+// new 
+const goToHome = () => {
+      router.push('/');
+    };
+
+const alertOn = () => {
+   showAlert.value = true;
+   console.log("onnn")
+};
+const alertOff = () => {
+   showAlert.value = false;
+   console.log("offf")
+};
 </script>
 
-<template>
-    <div class="p-4 mt-4 text-center">
-        <div class="flex flex-col">
-            <h4 class="text-2xl font-medium">
-                {{ guide.steps.length }}
-                {{ guide.steps.length === 1 ? getMessage("step") : getMessage("steps") }}
-            </h4>
-        </div>
-        <ul class="mt-4 flex flex-col gap-6">
-            <li v-if="step">
-                <p class="text-xl mb-2">
-                    {{ `${getMessage("step").toUpperCase()} ${step.counter}` }}
-                </p>
-                <p class="text-lg font-semibold mb-2">{{ step.title }}</p>
-                <p class="mb-2">{{ step.description }}</p>
-                <div>
-                    <p v-if="step.actionUrl" class="mb-4 text-sm text-blue-600">
-                        <a :href="step.actionUrl" target="_blank">{{ step.actionUrl }}</a>
-                    </p>
-                    <p v-else class="mb-4 text-sm text-red-600">
-                        {{ getMessage("stepNoUrlMessage") }}
-                    </p>
+<style>
+.bg-yellow {
+    background-color: #F6C254;
+  }
+.app-nav__logo {
+  width: 75px;
+  height: 15px;
+}
+.bg-dark-yellow {
+  background-color: #FADA98;
+}
+.icon-voz {
+  position: absolute;
+  right: 25px;
+  z-index: 1;
+  cursor: pointer;
+}.icon-voz-stop {
+  position: absolute;
+  right: 22px;
+  z-index: 1;
+  cursor: pointer;
+}
+
+.btn-record, .btn-stop-record {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 10px;
+  border-radius: 25px;
+  font-size: 18px;
+  cursor: pointer;
+}
+
+.btn-record {
+  background-color: #ffffff;
+  color: #222;
+  border: 1px solid black;
+}
+
+.btn-stop-record {
+  position: relative;
+  border-radius: 9999px;
+  background-color: rgb(255, 255, 255);
+  color: rgb(25, 25, 25);
+  font-weight: 700;
+  font-size: 16px;
+  overflow: hidden;
+  border: 1px solid black;
+  box-shadow: 0 0 0 0 rgba(255, 0, 0, 0.7);
+  animation: pulse 2s infinite;
+}
+
+.btn-stop-record-animation {
+  box-shadow: 0 0 0 0 rgba(255, 0, 0, 0.7);
+  animation: pulse 2s infinite;
+}
+
+@keyframes pulse {
+  0% {
+    box-shadow: 0 0 0 0 rgb(255, 0, 0);
+  }
+  70% {
+    box-shadow: 0 0 0 10px rgba(255, 0, 0, 0);
+  }
+  100% {
+    box-shadow: 0 0 0 0 rgba(255, 0, 0, 0);
+  }
+}
+
+
+.btn-back,
+.btn-continue {
+  padding: 12px 24px;
+  font-size: 16px;
+  font-weight: 400;
+  border-radius: 9999px;
+  text-align: center;
+}
+
+.btn-back {
+  background-color: #FFF;
+  border: 1px solid #222;
+  color: #000000;
+}
+
+.btn-continue {
+  background-color: #041C42;
+  color: #ffffff;
+}
+
+
+</style>
+
+<template class="bg-yellow">
+    
+    <div class="p-4 bg-yellow min-h-screen">
+
+        <div class="flex justify-between items-center mb-6">
+    <!-- Logo SVG -->
+    <img src="../../public/assets/img/logo.svg" class="app-nav__logo">
+    <button @click="goToHome" style="color: #041C42; font-size: 14px;">Cerrar</button>
+</div>
+
+    <!-- Boton flotante -->
+    <button v-if="!state.recording"
+        @click="startRecording" class="fixed bottom-5 left-[24%] flex items-center bg-white shadow-lg rounded-full border border-black z-30">
+                <div class="p-2 flex items-center justify-center mr-2">
+                    <img src="../../public/assets/img/video.svg">  
                 </div>
-                <div class="relative">
+                <span class="font-medium" style="margin-right: 25px;">
+                    {{ getMessage("startRecording") }} 
+            </span>
+      </button>
+        <!-- Boton flotante -->
+      <button v-if="state.recording"
+        @click="stopRecording" class="btn-stop-record-animation fixed bottom-5 left-[24%] flex items-center bg-white shadow-lg rounded-full border border-black z-30">
+            <div class="p-2 flex items-center justify-center mr-2">
+                <img style="height: 32px;" src="../../public/assets/img/stop.svg">
+            </div>
+            <span class="font-medium" style="margin-right: 25px;">
+                {{ getMessage("stopRecording") }} 
+            </span>
+      </button>
+
+      <button @click="clearSteps" class="fixed bottom-[20px] left-5 w-12 h-12 flex items-center justify-center bg-white shadow-lg rounded-full border border-black z-30 hover:bg-gray-100">
+            <i class="fas fa-redo w-6 h-6" style="font-size: 22px;z-index: 999;"></i>
+        </button>
+
+        <button @click="alertOn" class="fixed bottom-[20px] right-5 w-12 h-12 flex items-center justify-center bg-white shadow-lg rounded-full border border-black z-30 hover:bg-gray-100">
+            <i class="fas fa-check w-6 h-6" style="font-size: 22px;z-index: 999;"></i>
+        </button>
+
+         <!-- Modal -->
+         <div v-if="showAlert" style="z-index: 9999;" class="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
+  <div style="border-radius: 20px !important;" class="bg-white rounded-lg p-6 w-3/4 md:w-1/2 lg:w-1/3 relative">
+    <!--<img style="width: 25px; position: absolute; right: 20px; top: 20px;" src="../../public/assets/img/escudo.png">-->
+    <h2 class="text-xl font-semibold mb-4">¿Estás seguro de continuar?</h2>
+    <p class="mb-4">Ahora podrás editar los pasos.</p>
+    <div class="flex justify-center mt-4">
+      <button class="btn-back text-center w-1/2 mr-2" @click="alertOff">Cancelar</button>
+      <button class="btn-continue text-center w-1/2 ml-2" @click="openEditor">Continuar</button>
+    </div>
+  </div>
+</div>
+
+
+    <div v-if="guide.steps.length > 0" class="bg-dark-yellow rounded-lg p-4 mb-6" style="border-radius: 20px;">
+        <!-- Navegación entre pasos -->
+        <div class="flex justify-between items-center mb-0" style="font-size: 13px; font-weight: 600;">
+        <button
+          @click="prevStep"
+          :disabled="currentStepIndex === 0"
+          class="text-dark-500 flex items-center"
+        >
+          <i class="fas fa-chevron-left mr-2" style="font-size: 32px;"></i>
+        </button>
+
+        <span v-if="guide.steps.length > 0">Paso {{ currentStepIndex + 1 }} <span v-if="guide.steps.length >= 2">de {{ guide.steps.length }}</span></span>
+
+        <button
+          @click="nextStep"
+          :disabled="currentStepIndex >= guide.steps.length - 1"
+          class="text-dark-500 flex items-center">
+          <i class="fas fa-chevron-right ml-2" style="font-size: 32px; font-weight: 900;"></i>
+        </button>
+      </div>
+     
+    </div>
+
+
+    <div style="border-radius: 20px 20px 20px 20px;" class="mt-4 bg-white rounded-xl mb-20">
+
+         <!-- Imagen de la tarjeta -->
+          <div class="relative flex" style="border-radius: 20px 20px 0 0;">
+            <ul style="border-radius: 20px 20px 0 0;">
+            <li v-if="step" style="border-radius: 20px 20px 0 0;">
+                <div class="relative" style="border-radius: 20px 20px 0 0;">
                     <img
                         :src="step.screenshotUrl"
                         class="w-full h-auto"
                         :alt="step.description"
                         @load="onImageLoad($event, currentStepIndex)"
+                        style="border-radius: 20px 20px 0 0;"
                     />
                     <div
                         v-if="step.focusData.radius > 0"
                         class="absolute z-10 top-0 left-0 w-full h-full bg-black bg-opacity-50"
                         :style="cutoutStyle(step.focusData, currentStepIndex)"
+                        style="border-radius: 20px 20px 0 0;"
                     ></div>
                 </div>
-                <div>
-                    <button @click="prevStep">Previo</button>
-                    <button @click="nextStep">Siguiente</button>
-                </div>
             </li>
-        </ul>
-        <div class="flex gap-2 justify-center mt-5">
-            <button
-                v-if="!state.recording"
-                @click="startRecording"
-                class="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600"
-            >
-                {{ getMessage("startRecording") }}
-            </button>
-            <button
-                v-if="state.recording"
-                @click="stopRecording"
-                class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600"
-            >
-                {{ getMessage("stopRecording") }}
-            </button>
-            <button
-                @click="openEditor"
-                class="bg-pink-600 text-white py-2 px-4 rounded hover:bg-pink-900"
-            >
-                {{ getMessage("openEditor") }}
-            </button>
-            <button
-                @click="clearSteps"
-                class="bg-red-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-            >
-                {{ getMessage("clearSteps") }}
-            </button>
-        </div>
+            </ul>
+          </div>
+          
+          <div class="p-4">
+          <!-- Contenido de la tarjeta -->
+          <div>
+              <div class="flex flex-wrap">
+                  <!-- Columna para la imagen -->
+                <div class="flex items-start">
+                    <img src="../../public/assets/img/web.svg" width="50px" alt="Ejemplo de pantalla" class="rounded-lg mr-2" />
+                    <div class="flex flex-col">
+                        <h4 class="text-lg font-semibold" style="font-size: 12px; color: #004079;">
+                        {{ `PASO ${currentStepIndex + 1}` }}
+                        </h4>
+                        <p class="text-lg font-semibold mb-2 break-words">
+                        {{ guide.steps[currentStepIndex]?.title || 'Título del paso' }}
+                        </p>
+                    </div>
+                </div>
+                  <!-- Columna para el texto -->
+                  <div class="flex-grow break-words">
+                      <p class="mb-2 break-words" style="font-size: 16px;">{{ guide.steps[currentStepIndex]?.description || 'Descripción detallada del paso' }}</p>
+                      <div>
+                          <p style="font-size: 14px;color: #6c6c6c;" v-if="guide.steps[currentStepIndex]?.actionUrl" class="mb-4 break-all">
+                              <a :href="guide.steps[currentStepIndex].actionUrl" target="_blank">{{ guide.steps[currentStepIndex].actionUrl }}</a>
+                          </p>
+                          <p style="font-size: 14px;" v-else class="mb-4">
+                              {{ getMessage("stepNoUrlMessage") }}
+                          </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+        
     </div>
+</div>
 </template>
